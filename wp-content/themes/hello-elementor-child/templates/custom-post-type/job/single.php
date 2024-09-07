1f1907fcb9a2ada973c6cbaa17539ef09967f1ad
meggira <kumbharanameghna@gmail.com>
2024-09-08 00:31:22 +0530

    job details page

--- wp-content/themes/hello-elementor-child/templates/custom-post-type/job/single.php
+++ wp-content/themes/hello-elementor-child/templates/custom-post-type/job/single.php
@@ -1,3 +1,115 @@
 <?php get_header(); ?>
-<p class="sample-line">hello, this is the product details page.</p>
+
+<section class="job-detail-main">
+    <div class="container-boston">
+        <h1>PRODUCTION MANAGER</h1>
+        <div class="boosten-bradcom">
+            <ul>
+                <li>
+                    CARRERS
+                </li>
+                <li>
+                    <a href="javascript:void(0);">
+                        PRODUCTION MANAGER
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</section>
+
+<section class="job-secondblock">
+    <div class="container-boston">
+        <div class="jobdetailsbox">
+             <div class="jobdetailsbox-left">
+                <h2>PRODUCTION MANAGER <span>WOBURN, USA</span></h2>
+                <p>
+                    Pad printing inks for any application from Boston Industrial Solutions, Inc. are made with safety and sustainability in mind. Our top-quality inks are available in single- and two-component types. We make silicone, UV-curable, and solvent-based printing inks following strict quality management standards. These inks are not only enhanced and fine-tuned to adhere to the toughest substrates (materials) in the world, but they are also made to exceed ATSM, medical, and mil-spec standards. Many industries and Fortune 500 companies use these inks for printing, marking, and decorating parts. Boston Industrial Solutions, Inc. inks work effectively with any pad printing equipment, regardless of the manufacturer. Additionally, these inks achieve excellent results every time. Furthermore, we are constantly innovating to meet and exceed ever-changing compliance, consumer, and industrial applications.
+                </p> 
+                <div class="jobdetailsboxwithlist">
+                    <h3>Duties and Responsibilities:</h3>    
+                    <ul>
+                        <li>
+                            • Perform installations and repairs of digital inkjet equipment at customer sites
+                        </li>
+                        <li>
+                            • Provide training to customers on proper operation and maintenance of equipment, digital file preparation, and RIP processing 
+                        </li>
+                        <li>
+                            • Remote customer support and troubleshooting, as well as on-site service as needed
+                        </li>
+                        <li>
+                            • Effectively communicate and obtain appropriate data needed to identify, analyze, and resolve service requirements
+                        </li>
+                        <li>
+                            • Clearly communicate problem solutions through database entries, written reports and verbal communication with customers and colleagues
+                        </li>
+                        <li>
+                            • Maintaining of demonstration and test facilities
+                        </li>
+                        <li>
+                            • Imaging of printed samples for customers and marketing
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="jobdetailsboxwithlist">
+                    <h3>Qualifications:</h3>    
+                    <ul>
+                        <li>
+                            • Associate Degree or equivalent vocational training in a related field
+                        </li>
+                        <li>
+                            • Basic Graphic Arts skills with working knowledge of Adobe Creative Suite 
+                        </li>
+                        <li>
+                            • Experience in servicing and operating inkjet printing or similar equipment in a print production environment
+                        </li>
+                        <li>
+                            • Ability to work independently as well as part of a team
+                        </li>
+                        <li>
+                            • Should possess strong collaborative abilities, to not only obtain assistance form colleagues but also provide support when needed
+                        </li>
+                        <li>
+                            • Must have good colour vision and attention to detail
+                        </li>
+                        <li>
+                            • Strong aptitude for customer relations and communication skills
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="jobdetailsbox-right">
+                <a href="javascript:void(0);">
+                    APPLY FOR THIS POSITION
+                </a>
+
+                <div class="weofferbox">
+                    <h4>We offer:</h4>
+                    <ul>
+                        <li>
+                            • Dynamic global organization with a history of innovation and strong product portfolio
+                        </li>
+                        <li>
+                            • Challenging environment combined with a supportive management structure
+                        </li>
+                        <li>
+                            • Career development and growth
+                        </li>
+                        <li>
+                            • Competitive salary and benefit package
+                        </li>
+                        <li>
+                            • Friendly work environment surrounded by dedicated and professional colleagues
+                        </li>
+                        <li>
+                            • Full training on all aspects of our inkjet machines with a training plan
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 <?php get_footer();
